12fa63faf9c574b010c01b1c55b45db4d6f049db
Richard Clayton <rclayton@bericotechnologies.com>
2012-08-04 16:44:38 -0400

    Pulled tables out into their own templates

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,23 +1,6 @@
 
 
 $(function(){
-	$("div.infopanel").livequery(
-		function(){
-			$("i", this).click(function(){
-				$i = $(this);
-				$table = $("table", $i.parent());
-				if($i.attr("class") == "icon-chevron-up"){
-					$table.fadeOut();
-					$i.removeClass("icon-chevron-up");
-					$i.addClass("icon-chevron-down");
-				} else {
-					$table.fadeIn();
-					$i.removeClass("icon-chevron-down");
-					$i.addClass("icon-chevron-up");
-				}
-			});
-		}, 
-		function(){});
 	
 	function single_data_load_autocomplete($el){
 		var collection = $el.attr("source");
